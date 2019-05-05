d8e81d32f39b700c7eb8fa6d88811421f41b3e80
XNM <artemx8zx@gmail.com>
2019-05-05 22:28:38 +0300

    Lab04 with Tests

--- src/main/java/ua/edu/chmnu/fks/oop/Lab04/Lab04.java
+++ src/main/java/ua/edu/chmnu/fks/oop/Lab04/Lab04.java
@@ -2,14 +2,33 @@ package ua.edu.chmnu.fks.oop.Lab04;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Lab04 {
     public static void main(String[] args) {
-
+        ArrayList<Integer> arrParams = new ArrayList<Integer>();
+        Integer[] param = {1, 0, -7, -6};
+        arrParams.addAll(Arrays.asList(param));
+        try {
+            Lab04.schemeGornger(arrParams).toArray();
+        }
+        catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 
-    public static ArrayList getDels(int n) {
-        ArrayList dels = new ArrayList();
+    public static ArrayList<Integer> getDels(int n) throws Exception {
+        ArrayList<Integer> dels = new ArrayList<Integer>();
+        if(n == 0) {
+            throw new Exception("Zero have infinity dividers");
+        }
+        if (n < 0) {
+            n = Math.abs(n);
+        }
+        if (n == 1) {
+            dels.add(1);
+            return dels;
+        }
         int l = n;
         for (int i = 1; i < l; i++) {
             if(n % i == 0) {
@@ -21,8 +40,8 @@ public class Lab04 {
         return dels;
     }
 
-    public static ArrayList getProbableRoots(Object[] delsq, Object[] delsp) {
-        ArrayList pr = new ArrayList();
+    public static ArrayList<Double> getProbableRoots(Object[] delsq, Object[] delsp) {
+        ArrayList<Double> pr = new ArrayList<Double>();
         for (int i = 0; i < delsq.length; i++) {
             for (int j = 0; j < delsp.length; j++) {
                 int delspInt = (int)delsp[j];
@@ -31,6 +50,7 @@ public class Lab04 {
                     double d = (double)delsqInt/delspInt;
                     if(!pr.contains(d)) {
                         pr.add(d);
+                        pr.add(d * -1.);
                     }
                 }
             }
@@ -38,21 +58,47 @@ public class Lab04 {
         return pr;
     }
 
-    public static void schemeGornerRec(ArrayList roots, int[] cof, ArrayList pr) {
-
+    public static void schemeGornerRec(ArrayList<Integer> cof, ArrayList<Double> pr, ArrayList<Double> roots, Integer indexRoot) {
+        if (cof.size() == 1) {
+            return;
+        }
+        ArrayList<Integer> newCof;
+        double newc = 0;
+        do {
+            if (indexRoot == pr.size())
+            {
+                return;
+            }
+            newCof = new ArrayList<Integer>();
+            newCof.add(cof.get(0));
+            for (int i = 1; i < cof.size(); i++) {
+                newc = (int)newCof.get(i - 1) * (double)pr.get(indexRoot) + (int)cof.get(i);
+                if (i == cof.size() - 1 && newc != 0) {
+                    indexRoot++;
+                }
+                else if (i == cof.size() - 1 && newc == 0) {
+                    roots.add(pr.get(indexRoot));
+                    indexRoot++;
+                }
+                else {
+                    newCof.add((int)newc);
+                }
+            }
+        } while (newc != 0);
+        schemeGornerRec(newCof, pr, roots, indexRoot);
     }
 
-    public static ArrayList schemeGornger(int[] cof) {
-        ArrayList roots = new ArrayList();
-        if (cof.length > 1) {
-            ArrayList dp = getDels(cof[0]);
-            ArrayList dq = getDels(cof[cof.length - 1]);
-            ArrayList pr = getProbableRoots(dq.toArray(), dp.toArray());
-            schemeGornerRec(roots, cof, pr);
+    public static ArrayList<Double> schemeGornger(ArrayList<Integer> cof) throws Exception {
+        if (cof.size() > 1) {
+            ArrayList<Double> roots = new ArrayList<Double>();
+            ArrayList<Integer> dp = getDels(cof.get(0));
+            ArrayList<Integer> dq = getDels(cof.get(cof.size() - 1));
+            ArrayList<Double> pr = getProbableRoots(dq.toArray(), dp.toArray());
+            schemeGornerRec(cof, pr, roots, 0);
             return roots;
         }
         else {
-            return null;
+            throw new Exception("Incorrect polinome");
         }
     }
 }
---
+++ src/test/java/ua/edu/chmnu/fks/oop/Lab04/Lab04Test.java
@@ -0,0 +1,120 @@
+package ua.edu.chmnu.fks.oop.Lab04;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.parallel.Execution;
+
+import java.lang.reflect.Array;
+import java.lang.reflect.Executable;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Lab04Test {
+
+    @Test
+    public void schemeGorngerTest1() throws Exception {
+        ArrayList<Double> arrExpected = new ArrayList<Double>();
+        arrExpected.add(1.5);
+        ArrayList<Integer> arrParams = new ArrayList<Integer>();
+        Integer[] param = {8, -36, 54, -27};
+        arrParams.addAll(Arrays.asList(param));
+        assertArrayEquals(arrExpected.toArray(), Lab04.schemeGornger(arrParams).toArray());
+    }
+
+    @Test
+    public void schemeGorngerTest2() throws Exception {
+        ArrayList<Double> arrExpected = new ArrayList<Double>();
+        Double[] expected = {-1., 3., -2.};
+        arrExpected.addAll(Arrays.asList(expected));
+        ArrayList<Integer> arrParams = new ArrayList<Integer>();
+        Integer[] param = {1, 0, -7, -6};
+        arrParams.addAll(Arrays.asList(param));
+        assertArrayEquals(arrExpected.toArray(), Lab04.schemeGornger(arrParams).toArray());
+    }
+
+    @Test
+    public void schemeGorngerTest3() throws Exception {
+        ArrayList<Double> arrExpected = new ArrayList<Double>();
+        Double[] expected = {2., 3., 4.};
+        arrExpected.addAll(Arrays.asList(expected));
+        ArrayList<Integer> arrParams = new ArrayList<Integer>();
+        Integer[] param = {1, -9, 26, -24};
+        arrParams.addAll(Arrays.asList(param));
+        assertArrayEquals(arrExpected.toArray(), Lab04.schemeGornger(arrParams).toArray());
+    }
+
+    @Test
+    public void schemeGorngerTest4() throws Exception {
+        ArrayList<Double> arrExpected = new ArrayList<Double>();
+        Double[] expected = {1., -1., 2., 0.5};
+        arrExpected.addAll(Arrays.asList(expected));
+        ArrayList<Integer> arrParams = new ArrayList<Integer>();
+        Integer[] param = {2, -5, 0, 5, -2};
+        arrParams.addAll(Arrays.asList(param));
+        assertArrayEquals(arrExpected.toArray(), Lab04.schemeGornger(arrParams).toArray());
+    }
+
+    @Test
+    public void schemeGorngerTest5() throws Exception {
+        ArrayList<Double> arrExpected = new ArrayList<Double>();
+        Double[] expected = {-0.5};
+        arrExpected.addAll(Arrays.asList(expected));
+        ArrayList<Integer> arrParams = new ArrayList<Integer>();
+        Integer[] param = {8, 12, 6, 1};
+        arrParams.addAll(Arrays.asList(param));
+        assertArrayEquals(arrExpected.toArray(), Lab04.schemeGornger(arrParams).toArray());
+    }
+
+    @Test
+    void getDelsTest1() throws Exception {
+        ArrayList<Integer> arrExpected = new ArrayList<Integer>();
+        Integer[] param = {27, 1, 9, 3};
+        arrExpected.addAll(Arrays.asList(param));
+        assertArrayEquals(arrExpected.toArray(), Lab04.getDels(27).toArray());
+    }
+
+    @Test
+    void getDelsTest2() throws Exception {
+        ArrayList<Integer> arrExpected = new ArrayList<Integer>();
+        Integer[] param = {8, 1, 4, 2};
+        arrExpected.addAll(Arrays.asList(param));
+        assertArrayEquals(arrExpected.toArray(), Lab04.getDels(8).toArray());
+    }
+
+    @Test
+    void getDelsTest3() throws Exception {
+        ArrayList<Integer> arrExpected = new ArrayList<Integer>();
+        Integer[] param = {27, 1, 9, 3};
+        arrExpected.addAll(Arrays.asList(param));
+        assertArrayEquals(arrExpected.toArray(), Lab04.getDels(-27).toArray());
+    }
+
+    @Test
+    void getDelsTest4() throws Exception {
+        ArrayList<Integer> arrExpected = new ArrayList<Integer>();
+        Integer[] param = {1};
+        arrExpected.addAll(Arrays.asList(param));
+        assertArrayEquals(arrExpected.toArray(), Lab04.getDels(1).toArray());
+    }
+
+    @Test
+    void getProbableRootsTest1() {
+        ArrayList<Double> arrExpected = new ArrayList<Double>();
+        Double[] param = {1., -1., 0.25, -0.25, 2., -2., 0.5, -0.5};
+        arrExpected.addAll(Arrays.asList(param));
+        Object[] param1 = {1, 2};
+        Object[] param2 = {1, 4};
+        assertArrayEquals(arrExpected.toArray(), Lab04.getProbableRoots(param1, param2).toArray());
+    }
+
+    @Test
+    void getProbableRootsTest2() {
+        ArrayList<Double> arrExpected = new ArrayList<Double>();
+        Double[] param = {2., -2., 1.5, -1.5};
+        arrExpected.addAll(Arrays.asList(param));
+        Object[] param1 = {4, 3};
+        Object[] param2 = {2, 2};
+        assertArrayEquals(arrExpected.toArray(), Lab04.getProbableRoots(param1, param2).toArray());
+    }
+}
